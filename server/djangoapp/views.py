57ece8e15c50fc3975d9272be24d3905b3b7a619
Sourav Singh <55342631+100ravSingh@users.noreply.github.com>
2025-12-26 10:03:21 +0000

    frontend

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -38,6 +38,13 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
+@csrf_exempt
+def logout_user(request):
+    logout(request)  # Terminate user session
+    data = {"userName": ""}  # Return empty username
+    return JsonResponse(data)
+
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
