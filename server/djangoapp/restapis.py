6146476efca2b62ae7140aa0b0d1e7f19cb8cb3d
100ravSingh <ss3225220@gmail.com>
2025-12-26 07:50:26 -0500

    new

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default="https://didactic-lamp-q4vjwgjpgrjh456g-3030.app.github.dev")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="https://sentianalyzer.24c21jy4pklt.us-south.codeengine.appdomain.cloud")
 
 def get_request(endpoint, **kwargs):
     params = ""
@@ -59,3 +59,22 @@ def add_review(request):
             return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
         return JsonResponse({"status":403,"message":"Unauthorized"})
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
