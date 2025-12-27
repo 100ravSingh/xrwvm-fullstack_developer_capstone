71517feb43a34413d4ef248f0cc8294e0b26ed3f
Sourav Singh <55342631+100ravSingh@users.noreply.github.com>
2025-12-27 07:59:41 +0000

    lint fix

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://laughing-rotary-phone-j7rvxwvqgp92q776-8000.app.github.dev']
-CSRF_TRUSTED_ORIGINS = ['https://laughing-rotary-phone-j7rvxwvqgp92q776-8000.app.github.dev']
+ALLOWED_HOSTS = [
+        'localhost',
+        'https://laughing-rotary-phone-j7rvxwvqgp92q776-8000.app.github.dev'
+                ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://laughing-rotary-phone-j7rvxwvqgp92q776-8000.app.github.dev'
+    ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,8 +98,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    'NAME':
+'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
